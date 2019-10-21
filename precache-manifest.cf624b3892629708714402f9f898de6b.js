bc39e9c5741e587fa87f1e78d4e5e60ca286432c
julien <juliencaron18@gmail.com>
2019-10-22 00:34:29 +0200

    Updates

---
+++ precache-manifest.cf624b3892629708714402f9f898de6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8f91eb4a6bbbe6d3bd541696a04cb9",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e060cbe0ce421fee348e",
+    "url": "/random-quote-machine/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "fe6e6f4078681136ceb2",
+    "url": "/random-quote-machine/static/css/main.5bdd382d.chunk.css"
+  },
+  {
+    "revision": "e060cbe0ce421fee348e",
+    "url": "/random-quote-machine/static/js/2.e30df7ef.chunk.js"
+  },
+  {
+    "revision": "fe6e6f4078681136ceb2",
+    "url": "/random-quote-machine/static/js/main.55472dd8.chunk.js"
+  },
+  {
+    "revision": "a1217defecffe873141b",
+    "url": "/random-quote-machine/static/js/runtime-main.6d700caf.js"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/random-quote-machine/static/media/twitter.7e145c9e.svg"
+  }
+]);
